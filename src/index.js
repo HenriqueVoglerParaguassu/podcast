451d2d2a42a1a9a952ecdd1f292953a4222deb58
Henrique Paraguassu <henriquevoglerparaguassu@gmail.com>
2021-02-22 12:26:45 -0400

    varias modificaçoes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+const { urlencoded } = require("express");
 const express = require("express");
+const path = require("path");
+
+const routes = require("./routes");
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+// configura as views como html
+app.set("views", path.join(path.dirname(__dirname), "views"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
+// nao sei direito a utilidade principal mas acostumei assim
+app.use(express.static("./public"));
+
+// nem precisa explicar
+app.use(routes);
+
+// receber requisições no body via post
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+
 app.listen(3030, () => {
-  console.log("Running on http://localhsot:3030/");
+  console.log("Running on http://localhost:3030/");
 });
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 const routes = require("express").Router();
 
-routes.get("/", (req, res, next) => {
-  res.render("index");
-});
+routes
+  .get("/", (req, res, next) => {
+    res.render("index");
+  })
+
+  .get("/doe", (req, res, next) => {
+    res.render("doe");
+  });
+
+module.exports = routes;
